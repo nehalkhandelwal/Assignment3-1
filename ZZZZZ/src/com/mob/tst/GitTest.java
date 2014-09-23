1cc0e5aa43b73ddc1503928ceab576e3cba5ba71
UtkarshBhavsar <utkarshkbhavsar@gmail.com>
2014-09-23 22:07:58 +0530

    a

--- ZZZZZ/src/com/mob/tst/GitTest.java
+++ ZZZZZ/src/com/mob/tst/GitTest.java
@@ -1,7 +1,7 @@
 package com.mob.tst;
 
 public class GitTest {
-  public static void main(String[] args) {
-    System.out.println("Git is fun");
-  }
-} 
+    public static void main(String[] args) {
+        System.out.println("Git is fun");
+    }
+}
